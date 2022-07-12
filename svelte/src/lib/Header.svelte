e97488210e6a961416c07beb1048163d14b1e5d9
Pedro Henrique Costa <pedrocosta99@gmail.com>
2022-07-12 20:15:43 -0300

    add Header  component

---
+++ svelte/src/lib/Header.svelte
@@ -0,0 +1,28 @@
+<script></script>
+
+<header>
+  <span class="header-txt">Trabalhe no Elo7</span>
+</header>
+
+<style>
+
+  header{
+    background-color: linear-gradient(90deg, rgba(0,0,0,0.39957990031950286) 0%, rgba(237,228,228,0) 50%, rgba(0,0,0,0.4023810207676821) 100%) ;
+    background: url("../assets/teste/foto-header.png") no-repeat center;
+    width: 100%;
+    height: 500px;
+    display: flex;
+    text-align: center;
+    justify-content:center;
+    align-items:center;
+  }
+
+  .header-txt{
+    font-size: 4em;
+    padding-top: 2.5em;
+    color: #fff;
+    text-shadow: 2px 2px rgb(32, 31, 31);
+
+  }
+
+</style>
