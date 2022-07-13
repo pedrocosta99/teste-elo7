435dfc63d2fb57a7902c9675822f07c6375fd697
Pedro Henrique Costa <pedrocosta99@gmail.com>
2022-07-13 18:08:45 -0300

    add Component for Team

---
+++ svelte/src/lib/Team.svelte
@@ -0,0 +1,31 @@
+<script>
+  import IMAGES from "./images";
+</script>
+
+<section class='team'>
+  <h3>CONHEÇA NOSSO TIME <br> FORA DE SÉRIE</h3>
+  <div>
+    <img src={IMAGES.camila} alt="Foto da Camila, equipe Elo7">
+    <img src={IMAGES.guto} alt="Foto do Guto, equipe Elo7">
+    <img src={IMAGES.david} alt="Foto do David, equipe Elo7">
+    <img src={IMAGES.beatriz} alt="Foto da Beatriz, equipe Elo7">
+  </div>
+</section>
+
+<style>
+.team{
+    background-color: rgb(236, 234, 234);
+    width: 100%;
+    margin: 0 0;
+    padding: 2em 0;
+  }
+
+  img{
+    margin: 2em 5px;
+  }
+
+  h3 {
+    color: #7D7873;
+  }
+
+</style>
