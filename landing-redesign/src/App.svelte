b855439159f42f425f4d03862e89b98fe6555ebf
Pedro Henrique Costa <pedrocosta99@gmail.com>
2022-07-14 18:24:47 -0300

    REDESIGN LANDING PAGE - Initial commit with sass

---
+++ landing-redesign/src/App.svelte
@@ -0,0 +1,31 @@
+<script></script>
+
+<main>
+
+  <p>Outer test</p>
+  <div>
+    Teste
+    <p>Inner Test</p>
+  </div>
+</main>
+
+
+
+<style src="./app.scss" lang="scss">
+
+  p{
+    color: white;
+  }
+
+  div{
+    height: 200px;
+    background-color: white;
+    color: $yellow;
+
+    > p{
+      color: black;
+    }
+  }
+
+
+</style>
