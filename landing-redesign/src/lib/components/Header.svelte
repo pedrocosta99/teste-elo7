5435703743a197327c8e1171144f58471acf7faf
Pedro Henrique Costa <pedrocosta99@gmail.com>
2022-07-18 08:35:48 -0300

    Style(header): title design outlined shadow

--- landing-redesign/src/lib/components/Header.svelte
+++ landing-redesign/src/lib/components/Header.svelte
@@ -7,14 +7,16 @@ import IMAGES from "../scripts/images";
 
   <img src={IMAGES.fotoHeader} alt="Foto equipe Elo7">
 
-  <div class="header-title-bg">
-    <span class="header-title">Trabalhe no Elo7</span>
+  <div class="header-title">
+    <span class="header-title-main">Trabalhe <br/> no Elo7</span>
+    <span class="header-title-shadow">Trabalhe <br/> no Elo7</span>
   </div>
 
   <p>
     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt reprehenderit magnam quas ratione quisquam,
     saepe ducimus velit, nihil vel, consequuntur temporibus consectetur laborum adipisci.
   </p>
+
 
   <hr class="line-break">
 
@@ -35,22 +37,63 @@ import IMAGES from "../scripts/images";
 
   .header-title{
     position: relative;
-    bottom: 60px;
-    font-size: 5em;
-    padding: 0 0.3em;
+    width: 100%;
+    height: 10em;
+    display: flex;
+    justify-content: center;
   }
 
-  .header-title-bg{
-    margin-top: 4em;
-    height: 3em;
-    background-color: $yellow;
-    color: $green;
-    text-shadow: 4px 3px $beige-bg;
+  .header-title-main{
+    position: absolute;
+    font-size: 90px;
+    font-weight: bold;
+    color: $yellow;
+    text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;
+    z-index: 2;
+  }
+
+  .header-title-shadow{
+    position: absolute;
+    top: 3%; left: 10.5%;
+    margin: 0;
+    z-index: 1;
+    font-size: 90px;
+    font-weight: bold;
+    color: white;
+    text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;
 
   }
 
   p{
     text-align: center;
+    max-width: 80vw;
   }
+
+  br{
+    display: none;
+  }
+
+  @media screen and (max-width: 800px){
+    br{
+      display: flex;
+    }
+
+    .header-title-shadow{
+      left: 29.8%;
+    }
+    .header-title{
+      margin-bottom: 10vh;
+      display: flex;
+      text-align: center;
+      line-height: 8em;
+    }
+  }
+
+  @media screen and (max-width: 450px){
+    .header-title{
+      scale: 0.85;
+    }
+  }
+
 
 </style>
